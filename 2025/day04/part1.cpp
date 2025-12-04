fb911111ef545f0d64db47386866d0d940e0cd53
Evan He <eh4359@rit.edu>
2025-12-04 18:17:38 -0500

    wow this day is really easy. O(n) for pt1. O(n) for pt2. the number of times the while loop traverse cannot exceed n

---
+++ 2025/day04/part1.cpp
@@ -0,0 +1,77 @@
+#include "aoc.hpp"
+
+bool isValid(int check_row, int check_col, int row, int col) {
+    if (check_row > row - 1) return false;
+    if (check_row < 0) return false;
+    if (check_col > col - 1) return false;
+    if (check_col < 0) return false;
+    return true;
+}
+
+bool isPaper(int row, int col, std::vector<std::vector<std::string>>& grid) {
+    if (grid[row][col] == "@") return true;
+    return false;
+}
+
+void canAccess(int check_row, int check_col, int& ans, std::vector<std::vector<std::string>>& grid) {
+    int rows = grid.size();
+    int cols = grid[0].size();
+    int count = 0;
+    if (grid[check_row][check_col] != "@") return;
+
+    if (isValid(check_row - 1, check_col, rows, cols) && isPaper(check_row - 1, check_col, grid)) count++;
+    if (isValid(check_row - 1, check_col + 1, rows, cols) && isPaper(check_row - 1, check_col + 1, grid)) count++;
+    if (isValid(check_row - 1, check_col - 1, rows, cols) && isPaper(check_row - 1, check_col - 1, grid)) count++;
+
+    if (isValid(check_row, check_col - 1, rows, cols) && isPaper(check_row, check_col - 1, grid)) count++;
+    if (isValid(check_row, check_col + 1, rows, cols) && isPaper(check_row, check_col + 1, grid)) count++;
+
+    if (isValid(check_row + 1, check_col, rows, cols) && isPaper(check_row + 1, check_col, grid)) count++;
+    if (isValid(check_row + 1, check_col - 1, rows, cols) && isPaper(check_row + 1, check_col - 1, grid)) count++;
+    if (isValid(check_row + 1, check_col + 1, rows, cols) && isPaper(check_row + 1, check_col + 1, grid)) count++;
+
+
+    if (count < 4) {
+        ans++;
+    }
+}
+
+int main() {
+    std::string raw = aoc::readStdinAll();
+    aoc::trim(raw);
+    if (raw.empty()) {
+        std::cerr << "Warning: no input on stdin.\n";
+    }
+    std::vector<std::string> lines = aoc::split(raw, "\n");
+
+    // build out a 2d representation of the input
+    std::vector<std::vector<std::string>> grid;
+    int lineNumber = 0;
+    for (std::string line : lines) {
+        grid.push_back(std::vector<std::string>());
+        for (char c : line) {
+            std::string cs = "";
+            cs += c;
+            grid[lineNumber].push_back(cs);
+        }
+        lineNumber++;
+    }
+
+    // for (auto x : grid)
+    //     for (auto j : x)
+    //         std::cout << j << " "; 
+    //     std::cout << "\n";
+
+    int ans = 0;
+    int rows = grid.size();
+    int cols = grid[0].size();
+    for (int line = 0; line < rows; line++) {
+        for (int paper = 0; paper < cols; paper++) {
+            canAccess(line, paper, ans, grid);
+        }
+    }
+
+    std::cout << ans;
+
+    return 0;
+}
