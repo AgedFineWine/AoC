31f68fca0a2919248bc3cf2ac95fabe72dd519d3
Evan He <eh4359@rit.edu>
2025-12-07 21:44:38 -0500

    day3. need more powerful split function

--- 2016/day03/part2.cpp
+++ 2016/day03/part2.cpp
@@ -1,15 +1,48 @@
 #include "aoc.hpp"
 
+bool is_triangle(std::vector<size_t>& legs) {
+    sort(legs.begin(), legs.end());
+    return legs[0] + legs[1] > legs[2];
+}
+
 int main() {
     std::string raw = aoc::read_stdin_all();
     aoc::trim(raw);
     if (raw.empty()) {
         std::cerr << "Warning: no input on stdin.\n";
     }
+    std::vector<std::string> lines = aoc::split(raw, "\n");
+    std::vector<std::vector<size_t>> grid;
+    for (std::string& x : lines) {
+        aoc::trim(x);
+        std::vector<std::string> v = aoc::split(x, "  ");
+        std::vector<size_t> legs;
+        for (std::string& j : v) {
+            try{
+                legs.push_back(std::stoull(j));
+            } catch (...) {
+                std::cout << "error:'"<< j<<"'";
+            }
+        }
+        grid.push_back(legs);
+    }
 
-    // TODO: implement Part 1 and Part 2
-    std::cout << "Part 1: " << "<answer>" << "\n";
-    std::cout << "Part 2: " << "<answer>" << "\n";
+    int col = 0;
+    size_t ans = 0;
+    std::vector<size_t> buffer;
+    for (int j = 0; j < 3; j++) {
+        col = 0;
+        for (size_t i = 0; i < grid.size(); i++) {
+            if (buffer.size() != 3)
+                buffer.push_back(grid[i][col]);
+            if (is_triangle(buffer)) {
+                ans++;
+                buffer.clear();
+            }
+        }
+    }
+
+    std::cout << ans;
 
     return 0;
 }
